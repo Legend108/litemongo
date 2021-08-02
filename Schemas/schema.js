faa1736b58f06390e609522221f4e07a02a8e57c
Legend108 <80154701+Legend108@users.noreply.github.com>
2021-08-02 10:13:55 +0530

    Create schema.js

---
+++ Schemas/schema.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  key: mongoose.SchemaTypes.String,
+  value: mongoose.SchemaTypes.String,
+});
+
+module.exports = mongoose.model("ScyllaDB", schema);
