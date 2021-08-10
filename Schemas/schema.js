d07e283947b2d80201c15db3bf676dc6182c495a
Legend108 <80154701+Legend108@users.noreply.github.com>
2021-08-10 16:38:54 +0530

    Now you can store booleans, numbers, etc. in the values

--- Schemas/schema.js
+++ Schemas/schema.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
   _id: mongoose.SchemaTypes.String,
-  value: mongoose.SchemaTypes.String,
+  value: mongoose.SchemaTypes.Mixed,
 });
 
 module.exports = mongoose.model("sMongoose", schema);
+
