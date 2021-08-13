2e9a05a80f5fded22a9b5afecaca4353e51313e5
Legend108 <80154701+Legend108@users.noreply.github.com>
2021-08-13 20:01:14 +0530

    Perfectly fine code, just doesnt work

--- Structures/Core.js
+++ Structures/Core.js
@@ -11,13 +11,13 @@ class database {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useFindAndModify: false,
-    })
+    });
   }
 
   /**
    * Set value to a key
-   * @param {string} key 
-   * @param {string} value 
+   * @param {string} key
+   * @param {string} value
    */
   async set(key, value) {
     if (!key) throw new Error("Invalid key type", "databaseError");
@@ -29,17 +29,7 @@ class database {
     if (!value) throw new Error("Invalid value type", "databaseError");
 
     await schema
-      .findOneAndUpdate(
-        {
-          _id: key,
-        },
-        {
-          value: value,
-        },
-        {
-          upsert: true,
-        }
-      )
+      .findOneAndUpdate({ _id: key }, { value: value }, { upsert: true })
       .catch((err) => {
         console.log(err);
       });
@@ -47,7 +37,7 @@ class database {
 
   /**
    * Find a key
-   * @param {string} key 
+   * @param {string} key
    * @returns {{ key: string, value: string }}
    */
   async find(key) {
@@ -55,7 +45,7 @@ class database {
 
     let val = await schema.findById(key);
 
-    return val;
+    return val.value;
   }
 
   /**
@@ -70,8 +60,8 @@ class database {
 
   /**
    * Delete an key
-   * @param {string} key 
-   * @returns {{ key: string, value: string }}
+   * @param {string} key
+   * @returns {{ key: string, value: any }}
    */
   async delete(key) {
     if (!key) throw new Error("The key has to be provided", "databaseError");
@@ -84,6 +74,26 @@ class database {
   async clear() {
     await schema.deleteMany();
     // Added the clear function
+  }
+
+  async add(key, number) {
+    if (!key) throw new Error("The key has to be provided!", "databaseError");
+    if (!number)
+      throw new Error("The number to add has to be provided", "databaseError");
+    if (typeof number !== "number")
+      throw new Error(
+        "The typeof the number to add has to be a number, received " +
+          typeof number
+      );
+
+    this.find(key)
+      .then((v) => {
+        this.set(key, number + v);
+        return true;
+      })
+      .catch((e) => {
+        throw new Error(e);
+      });
   }
 }
 
